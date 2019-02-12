e7fae4da5dae8e54018f8144b9104018818c9b2e
Matthew Cook <mc255v@gmail.com>
2019-02-12 19:45:08 +0900

    added seasons folder with basic structure

---
+++ seasons/src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = { lat: null, errorMessage: '' };
+        
+         window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({ lat: position.coords.latitude });
+            },
+            (error) => {
+                this.setState({ errorMessage: error.message });
+            }
+        );
+    }
+    
+    render() {
+        return (
+            <div>
+                Latitude: {this.state.lat}
+                <br />
+                Error: {this.state.errorMessage}
+            </div>    
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
