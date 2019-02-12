d82eeb750c2c6c8c9733eaef73b84715aa617aab
Matthew Cook <mc255v@gmail.com>
2019-02-12 21:35:21 +0900

    loading screen and season display added

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,30 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        
-        this.state = { lat: null, errorMessage: '' };
-        
-         window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                this.setState({ lat: position.coords.latitude });
-            },
-            (error) => {
-                this.setState({ errorMessage: error.message });
-            }
+    state = { lat: null, errorMessage: '' };
+
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({ lat: position.coords.latitude }),
+            error => this.setState({ errorMessage: error.message })
         );
     }
     
     render() {
-        return (
-            <div>
-                Latitude: {this.state.lat}
-                <br />
-                Error: {this.state.errorMessage}
-            </div>    
-        );
+        if(this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+
+        if(!this.state.errorMessage && this.state.lat) {
+            return <SeasonDisplay lat={this.state.lat}/>
+        }
+
+        return <div><Spinner message="Please accept the location request..."/></div>;
     }
 }
 
