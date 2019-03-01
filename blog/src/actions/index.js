238ebbd059344e91f6a7036d9d8413d1a78e5c89
Matthew C <dragokaiden@gmail.com>
2019-03-01 09:04:35 +0000

    refactor of reducers using lodash

---
+++ blog/src/actions/index.js
@@ -0,0 +1,32 @@
+import _ from 'lodash';
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPosts = () => {
+    return async dispatch => {
+        const response = await jsonPlaceholder.get('/posts');
+        
+        dispatch({
+            type: 'FETCH_POSTS',
+            payload: response.data
+        });
+    }
+};
+
+// export const fetchUser = id => {
+//     return async dispatch => {
+//         const response = await jsonPlaceholder.get(`/users/${id}`);
+        
+//         dispatch({
+//             type: 'FETCH_USER',
+//             payload: response.data
+//         });
+//     }
+// }
+
+export const fetchUser = function(id) { 
+     return _.memoize(async function(dispatch) {
+        const response = await jsonPlaceholder.get(`/users/${id}`);
+        
+        dispatch({ type: 'FETCH_USER', payload: response.data });
+    });
+};
